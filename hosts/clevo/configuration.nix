235c576371e77e2c48666f6c06f84437ce84c99e
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-05 08:53:35 +0200

    Initial setup

---
+++ hosts/clevo/configuration.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  systemd.enableEmergencyMode = false;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.kernelParams = [ "nohibernate" ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostId = "13eb44cc";
+  networking.hostname = "clevo";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Brussels";
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    curl
+    git
+  ];
+
+  users.users.grizz = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    packages = [ ];
+  };
+
+  system.stateVersion = "23.05";
+}
