19f77237e6fe77719ee20f0fe09a6900a6eb90bd
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-05 10:31:02 +0200

    Change permission on system-connections

--- hosts/clevo/persist.nix
+++ hosts/clevo/persist.nix
@@ -8,7 +8,7 @@
       "/var/lib/nixos"
       "/var/lib/systemd/coredump"
       "/etc/nixos"
-      "/etc/NetworkManager/system-connections"
+      { directory = "/etc/NetworkManager/system-connections"; user = "root"; group = "root"; mode = "u=rwx,g=,o="; }
       { directory = "/etc/shadow"; user = "root"; group = "shadow"; mode = "u=rw,g=r,o="; }
     ];
     files = [
