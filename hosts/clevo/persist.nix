e2e18856a7de8086b91d064055870b01a7e780f3
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-05 10:36:53 +0200

    Change /etc/shadow

--- hosts/clevo/persist.nix
+++ hosts/clevo/persist.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   environment.persistence."/persist" = {
-    # hideMounts = true;
+    hideMounts = true;
     directories = [
       "/var/log"
       "/var/lib/bluetooth"
@@ -9,10 +9,10 @@
       "/var/lib/systemd/coredump"
       "/etc/nixos"
       { directory = "/etc/NetworkManager/system-connections"; user = "root"; group = "root"; mode = "u=rwx,g=,o="; }
-      { directory = "/etc/shadow"; user = "root"; group = "shadow"; mode = "u=rw,g=r,o="; }
     ];
     files = [
       "/etc/machine-id"
+      { file = "/etc/shadow"; parentDirectory = { user = "root"; group = "shadow"; mode = "u=rw,g=r,o="; }; }
     ];
   };
 }
