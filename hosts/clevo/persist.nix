57ad21928a0925886c17626227cded13e11af0a6
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-05 12:54:27 +0200

    Unlink for now

--- hosts/clevo/persist.nix
+++ hosts/clevo/persist.nix
@@ -12,10 +12,10 @@
     ];
   };
 
-  environment.etc."machine-id".source = "/persist/etc/machine-id";
-  environment.etc."group".source = "/persist/etc/group";
-  environment.etc."shadow".source = "/persist/etc/shadow";
-  environment.etc."passwd".source = "/persist/etc/passwd";
-  environment.etc."subgid".source = "/persist/etc/subgid";
-  environment.etc."subuid".source = "/persist/etc/subuid";
+  # environment.etc."machine-id".source = "/persist/etc/machine-id";
+  # environment.etc."group".source = "/persist/etc/group";
+  # environment.etc."shadow".source = "/persist/etc/shadow";
+  # environment.etc."passwd".source = "/persist/etc/passwd";
+  # environment.etc."subgid".source = "/persist/etc/subgid";
+  # environment.etc."subuid".source = "/persist/etc/subuid";
 }
