5511c33490ca949587fa0cd49296e5ff890ac380
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-06 20:50:40 +0200

    Remove facebook + add hyprland cachix

--- hosts/clevo/configuration.nix
+++ hosts/clevo/configuration.nix
@@ -15,11 +15,25 @@ in
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    settings = {
+      auto-optimise-store = true;
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   networking.hostId = hostId;
   networking.hostName = hostName;
   networking.networkmanager.enable = true;
+  networking.extraHosts = ''
+    0.0.0.0 facebook.com m.facebook.com
+  '';
+
   security.sudo.extraConfig = "Defaults lecture = never";
 
   time.timeZone = "Europe/Brussels";
