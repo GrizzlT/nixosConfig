9e17659ee3bd918aea4e3fcfe17a703220cd5c91
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-08 23:24:05 +0200

    Wezterm - seems good enough now

--- hosts/clevo/home/wezterm/wez-conf/colors.lua
+++ hosts/clevo/home/wezterm/wez-conf/colors.lua
@@ -2,48 +2,21 @@ local wezterm = require('wezterm')
 local M = {}
 
 M.apply_tab_colors = function (config)
-  local selected_scheme = "tokyonight";
-  local scheme = wezterm.get_builtin_color_schemes()[selected_scheme]
-
-  local C_ACTIVE_BG = scheme.background;
-  local C_ACTIVE_FG = scheme.foreground;
-  local C_BG = scheme.brights[0];
-  local C_INACTIVE_FG = scheme.ansi[7];
-
-  scheme.tab_bar = {
-    background = C_BG,
-    new_tab = {
-      bg_color = C_BG,
-      fg_color = C_ACTIVE_BG,
-    },
-    new_tab_hover = {
-      bg_color = C_BG,
-      fg_color = C_ACTIVE_BG,
-    },
-    active_tab = {
-      bg_color = C_ACTIVE_BG,
-      fg_color = C_ACTIVE_FG,
-    },
-    inactive_tab = {
-      bg_color = C_BG,
-      fg_color = C_INACTIVE_FG,
-    },
-    inactive_tab_hover = {
-      bg_color = C_BG,
-      fg_color = C_INACTIVE_FG,
-    }
-  }
+  local C_ACTIVE_BG = "#282a36";
+  local C_ACTIVE_FG = "#eff0eb";
+  local C_BG = "#686868";
+  local C_INACTIVE_FG = "#f1f1f0";
 
   wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
     if tab.is_active then
       return {
+        {Background={Color=C_ACTIVE_BG}},
         {Foreground={Color=C_ACTIVE_FG}},
         {Text=" " .. tab.tab_index+1},
         {Foreground={Color=C_ACTIVE_FG}},
         {Text=": "},
         {Foreground={Color=C_ACTIVE_FG}},
         {Text=tab.active_pane.title .. " "},
-        {Background={Color=C_ACTIVE_BG}},
       }
     end
     return {
@@ -58,13 +31,9 @@ M.apply_tab_colors = function (config)
 
   config.window_frame = {
     font = wezterm.font { family = 'Hack Nerd Font Mono', weight = 'Bold' },
-    active_titlebar_bg = "#686868",
-    inactive_titlebar_bg = "#686868",
+    active_titlebar_bg = C_BG,
+    inactive_titlebar_bg = C_BG,
   }
-  config.color_schemes = {
-    [selected_scheme] = scheme
-  }
-  config.color_scheme = selected_scheme
 end
 
 return M
