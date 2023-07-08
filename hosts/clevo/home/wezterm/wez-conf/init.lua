3586a5510d25bea6931d8e7d0a8bdfe64f721737
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-08 22:15:34 +0200

    Specify correct font name

--- hosts/clevo/home/wezterm/wez-conf/init.lua
+++ hosts/clevo/home/wezterm/wez-conf/init.lua
@@ -3,7 +3,7 @@ local wezterm = require('wezterm')
 local M = {}
 
 M.apply_to_config = function (config)
-  config.font = wezterm.font 'Hack'
+  config.font = wezterm.font 'Hack Nerd Font Mono'
   config.harfbuzz_features = {}
 end
 
