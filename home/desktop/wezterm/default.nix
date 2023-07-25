57fc22d21fdc88cc0998166ba1d1d4bd96b3c2a4
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-25 20:00:21 +0200

    Fix lua syntax issue

--- home/desktop/wezterm/default.nix
+++ home/desktop/wezterm/default.nix
@@ -4,9 +4,9 @@
     enable = true;
     extraConfig = ''
       return {
-        harfbuzz_features = {}
-        disable_default_key_bindings = true
-        key_map_preference = "Mapped"
+        harfbuzz_features = {},
+        disable_default_key_bindings = true,
+        key_map_preference = "Mapped",
       }
     '';
   };
