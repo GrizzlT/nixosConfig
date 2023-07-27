60c0f40a432e46a9cab1048b8ae81be57d895d26
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-27 17:34:01 +0200

    Add git aliases

--- home/base/zsh/default.nix
+++ home/base/zsh/default.nix
@@ -1,82 +1,77 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ../exa.nix
   ];
 
-  config = lib.mkMerge [
-    {
-      home.packages = with pkgs; [
-        fd
-        zsh-powerlevel10k
-      ];
-
-      programs.zoxide = {
-        enable = true;
-        enableZshIntegration = true;
-        enableBashIntegration = true;
-      };
-
-      programs.zsh = {
-        enable = true;
-        enableAutosuggestions = true;
-        enableCompletion = true;
-        defaultKeymap = "emacs";
-        envExtra = ''
-          GPG_TTY=$TTY
-          setopt no_global_rcs
-        '';
-        shellAliases = {
-          ls = "exa";
-          cd = "z";
-          systemUpdate = "sudo nixos-rebuild switch --flake";
-          homeUpdate = "home-manager switch --flake";
-          weMain = "${pkgs.curl}/bin/curl wttr.in/Leuven";
-          weSecond = "${pkgs.curl}/bin/curl wttr.in/Bonheiden";
-        };
-        history = {
-          ignoreDups = true;
-          ignoreSpace = true;
-          share = true;
-          save = 10000;
-        };
-        plugins = [
-          {
-            name = "fast-syntax-highlighting";
-            src = pkgs.fetchFromGitHub {
-              owner = "zdharma-continuum";
-              repo = "fast-syntax-highlighting";
-              rev = "cf318e06a9b7c9f2219d78f41b46fa6e06011fd9";
-              sha256 = "RVX9ZSzjBW3LpFs2W86lKI6vtcvDWP6EPxzeTcRZua4=";
-            };
-          }
-        ];
-        initExtraFirst = ''
-          # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-          # Initialization code that may require console input (password prompts, [y/n]
-          # confirmations, etc.) must go above this block; everything else may go below.
-          if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-            source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-          fi
-        '';
-        initExtra = ''
-          bindkey '^ ' autosuggest-accept
-
-          source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-
-          # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-          [[ ! -f ${./p10k.zsh} ]] || source ${./p10k.zsh};
-        '';
-      };
-    }
-    {
-      programs.zsh.initExtra = lib.mkAfter ''
-        source ${pkgs.fetchFromGitHub {
-          owner = "changyuheng";
-          repo = "fz.sh";
-          rev = "7b4e215f5887b24e1ef725ffdb89f3479e913875";
-        }}/fz.plugin.zsh
-      '';
-    }
+  home.packages = with pkgs; [
+    fd
+    zsh-powerlevel10k
   ];
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    defaultKeymap = "emacs";
+    envExtra = ''
+      GPG_TTY=$TTY
+      setopt no_global_rcs
+    '';
+    shellAliases = {
+      ls = "exa";
+      cd = "z";
+      systemUpdate = "sudo nixos-rebuild switch --flake";
+      homeUpdate = "home-manager switch --flake";
+      weMain = "${pkgs.curl}/bin/curl wttr.in/Leuven";
+      weSecond = "${pkgs.curl}/bin/curl wttr.in/Bonheiden";
+    };
+    history = {
+      ignoreDups = true;
+      ignoreSpace = true;
+      share = true;
+      save = 10000;
+    };
+    plugins = [
+      {
+        name = "fast-syntax-highlighting";
+        src = pkgs.fetchFromGitHub {
+          owner = "zdharma-continuum";
+          repo = "fast-syntax-highlighting";
+          rev = "cf318e06a9b7c9f2219d78f41b46fa6e06011fd9";
+          sha256 = "RVX9ZSzjBW3LpFs2W86lKI6vtcvDWP6EPxzeTcRZua4=";
+        };
+      }
+      {
+        name = "git";
+        src = pkgs.fetchFromGitHub {
+          owner = "davidde";
+          repo = "git";
+          rev = "b60e5e36c65f76a29eeacbfb7f5de1cd00c86115";
+        };
+      }
+    ];
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
+    initExtra = ''
+      bindkey '^ ' autosuggest-accept
+
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+      # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+      [[ ! -f ${./p10k.zsh} ]] || source ${./p10k.zsh};
+    '';
+  };
 }
