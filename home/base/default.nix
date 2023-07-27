c596927a6cd0073d5bd6401c13c5a3146b41ed07
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-27 14:38:10 +0200

    Configure powerlevel10k

--- home/base/default.nix
+++ home/base/default.nix
@@ -3,7 +3,7 @@
   imports = [
     ./style.nix
 
-    ./zsh.nix
+    ./zsh
   ];
 
   programs = {
--- home/base/zsh/default.nix
+++ home/base/zsh/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   imports = [
-    ./exa.nix
+    ../exa.nix
   ];
 
   home.packages = with pkgs; [
@@ -36,8 +36,18 @@
       share = true;
       save = 10000;
     };
+    initExtraFirst = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
     initExtra = ''
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     '';
   };
+
+  home.file.".p10k.zsh".source = ./p10k.zsh;
 }
