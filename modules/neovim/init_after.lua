ddffaae087e5b7bcc37a47dc97f6580e0363a1b7
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-07-29 14:50:23 +0200

    Add autocommands

--- modules/neovim/init_after.lua
+++ modules/neovim/init_after.lua
@@ -1,3 +1,5 @@
+local GrizzGroup = vim.api.nvim_create_augroup('Grizz', {})
+
 -- Highlight a selection on Yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_on_yank", {}),
@@ -7,3 +9,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     })
   end
 })
+
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  group = GrizzGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = GrizzGroup,
+  pattern = { "/tmp/*", "COMMIT_EDITMSG", "MERGE_MSG", "*.tmp", "*.bak", "/private/*", },
+  callback = function()
+    vim.opt_local.undofile = false
+    vim.opt_local.writebackup = false
+    vim.opt_local.backup = false
+    vim.opt_local.swapfile = false
+  end,
+})
+
