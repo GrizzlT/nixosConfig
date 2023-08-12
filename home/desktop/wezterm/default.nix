062634b97b8bf6f6ebaaa3e8b945e162b13753f0
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2023-08-12 15:25:21 +0200

    Add nix tab width + wezterm cursor fg

--- home/desktop/wezterm/default.nix
+++ home/desktop/wezterm/default.nix
@@ -5,6 +5,12 @@
     extraConfig = ''
       return {
         harfbuzz_features = {},
+
+        tab_bar_at_bottom = true,
+        colors = {
+          cursor_fg = 'rgba(40, 42, 54)',
+        },
+
         disable_default_key_bindings = true,
         key_map_preference = "Mapped",
       }
