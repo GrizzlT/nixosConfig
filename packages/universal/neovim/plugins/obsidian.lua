61be6c13ae87d99b08d3e75ecc4b5e5976be8838
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2024-02-07 14:28:30 +0100

    chore(obsidian): set daily notes dir

--- packages/universal/neovim/plugins/obsidian.lua
+++ packages/universal/neovim/plugins/obsidian.lua
@@ -47,6 +47,16 @@ require('obsidian').setup({
   end,
 
   notes_subdir = "notes",
+  daily_notes = {
+    -- Optional, if you keep daily notes in a separate directory.
+    folder = "notes/dailies",
+    -- Optional, if you want to change the date format for the ID of daily notes.
+    date_format = "%Y-%m-%d",
+    -- Optional, if you want to change the date format of the default alias of daily notes.
+    alias_format = "%B %-d, %Y",
+    -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+    template = nil
+  },
   completion = {
     new_notes_location = "notes_subdir",
   },
