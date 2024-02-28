604988b635ba0b6f95164efd8d3715a452769cc6
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2024-02-28 20:55:03 +0100

    neovim: update lazy plugins

--- nvim/lua/grizz/plugins/autopairs.lua
+++ nvim/lua/grizz/plugins/autopairs.lua
@@ -2,7 +2,34 @@ return {
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = true,
-    enabled = false, -- TODO: fix config before enabling
+    config = function ()
+      local npairs = require('nvim-autopairs')
+      local Rule = require('nvim-autopairs.rule')
+      local cond = require('nvim-autopairs.conds')
+
+      npairs.setup({
+        check_ts = true,
+        fast_wrap = {},
+      })
+
+      for _, punct in ipairs({ ",", ";" }) do
+        npairs.add_rule(
+          Rule('', punct)
+            :with_move(function (opts) return opts.char == punct end)
+            :with_pair(cond.none())
+            :with_del(cond.none())
+            :with_cr(cond.none())
+            :use_key(punct)
+        )
+      end
+
+      local ok, cmp = pcall(require, 'cmp')
+      if ok then
+        cmp.event:on(
+          "confirm_done",
+          require('nvim-autopairs.completion.cmp').on_confirm_done()
+        )
+      end
+    end,
   },
 }
