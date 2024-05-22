6ed2671d00818aff4a7ad8c8e7bd3a73ffcb03cd
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2024-05-22 18:31:17 +0200

    chore: update unstable

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    unstable.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       # url = "https://flakehub.com/f/nix-community/home-manager/0.2311.tar.gz";
