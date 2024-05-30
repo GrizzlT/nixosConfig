00abbc60a0f7c963a503797ad200e6d6a47381b2
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2024-05-30 22:39:15 +0200

    ci: use old method

--- hm.nix
+++
@@ -1,7 +0,0 @@
-let
-  outputs = builtins.getFlake (toString ./.);
-  # pkgs = outputs.inputs.nixpkgs;
-  # drvs = pkgs.lib.collect pkgs.lib.isDerivation outputs.hydraJobs;
-in [
-  outputs.homeConfigurations."grizz@clevo".activationPackage
-]
