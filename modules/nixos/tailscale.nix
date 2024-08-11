c27175fb2306dea4c759e01d86f1907419ac12c0
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2024-08-11 21:43:06 +0200

    chore: tailscale force nftables

--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -6,4 +6,8 @@
   };
 
   systemd.services.tailscaled.after = [ "network.target" "systemd-resolved.service" ];
+
+  systemd.services.tailscaled.serviceConfig.Environment = [
+    "TS_DEBUG_FIREWALL_MODE=nftables"
+  ];
 }
