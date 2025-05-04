f30b29f6fec6803b86e8e16889d405a49ac172f9
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-05-04 15:06:17 +0200

    feat: add taskwarrior + zk

--- modules/hm/headless/productivity.nix
+++ modules/hm/headless/productivity.nix
@@ -7,6 +7,9 @@
     hledger
     hledger-web
     hledger-utils
+
+    taskwarrior3
+    zk
   ];
 
   home.sessionVariables = { LEDGER_FILE = "$HOME/DATA/01_Administration/hledger/2024.journal"; };
