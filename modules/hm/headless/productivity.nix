1da46f80933be546bbff729d8dcfdbd4429a5e03
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-05-04 16:24:39 +0200

    feat: update zk config

--- modules/hm/headless/productivity.nix
+++ modules/hm/headless/productivity.nix
@@ -12,5 +12,8 @@
     zk
   ];
 
-  home.sessionVariables = { LEDGER_FILE = "$HOME/DATA/01_Administration/hledger/2024.journal"; };
+  home.sessionVariables = {
+    LEDGER_FILE = "$HOME/DATA/01_Administration/hledger/2024.journal";
+    ZK_NOTEBOOK_DIR = "$HOME/DATA/02_Informational/Zettelkasten/Notebook";
+  };
 }
