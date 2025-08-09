2178c55c5ed17d2192a95e302ad4677cba2c5b46
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-08-09 13:51:29 +0200

    chore: fix picotool

--- hosts/clevo/udev.nix
+++ hosts/clevo/udev.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }:
 {
-  services.udev.packages = [ pkgs.qmk-udev-rules pkgs.stlink pkgs.qFlipper pkgs.android-udev-rules pkgs.pictool ];
+  services.udev.packages = [ pkgs.qmk-udev-rules pkgs.stlink pkgs.qFlipper pkgs.android-udev-rules pkgs.picotool ];
 }
