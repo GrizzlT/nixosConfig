468c78bebdaf59ffbccecf53a00a14ca7e450121
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-12-04 22:06:42 +0100

    feat: age recipients

--- modules/hm/headless/passage.nix
+++ modules/hm/headless/passage.nix
@@ -3,6 +3,7 @@
   home.sessionVariables = {
     PASSAGE_DIR = "$HOME/DATA/.passage/store";
     PASSAGE_IDENTITIES_FILE = "$HOME/DATA/.passage/identities";
+    PASSAGE_RECIPIENTS_FILE = "$HOME/DATA/.passage/store/.age-recipients";
     PASSWORD_STORE_CLIP_TIME = 20;
     PASSWORD_STORE_GENERATED_LENGTH = 20;
   };
