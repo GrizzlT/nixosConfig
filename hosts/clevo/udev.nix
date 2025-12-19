986967396ff5f24c53c0482747529ec0b8df6d7a
GrizzlT <13691001+GrizzlT@users.noreply.github.com>
2025-12-20 00:29:14 +0100

    feat: upgrade snowcicles

--- hosts/clevo/udev.nix
+++ hosts/clevo/udev.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   services.udev = {
-    packages = [ pkgs.qmk-udev-rules pkgs.stlink pkgs.qFlipper pkgs.android-udev-rules ];
+    packages = [ pkgs.qmk-udev-rules pkgs.stlink pkgs.qFlipper ];
     extraRules = ''
       SUBSYSTEM=="usb", ATTRS{idVendor}=="2e8a", ATTRS{idProduct}=="0003", TAG+="uaccess", MODE="660", GROUP="wheel"
       SUBSYSTEM=="block", ENV{ID_VENDOR_ID}=="2e8a", ENV{ID_MODEL_ID}=="0003", GROUP="wheel", MODE="0660"
